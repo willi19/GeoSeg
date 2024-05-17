83f53aca576a1256a25a93bf6dc818caf08c81e6
willi19 <willi19@naver.com>
2024-05-17 12:07:45 +0900

    init

---
+++ num_param.py
@@ -0,0 +1,19 @@
+from tools.cfg import py2cfg
+import os
+import torch
+from torch import nn
+import cv2
+import numpy as np
+import argparse
+from pathlib import Path
+from tools.metric import Evaluator
+from pytorch_lightning.loggers import CSVLogger
+import random
+
+config = py2cfg('GeoSeg/config/loveda/unetformer.py')
+net = config.net
+
+print("GeoSeg : ", sum(p.numel() for p in net.parameters() if p.requires_grad))
+print(net)
+print("Backbone : ", sum(p.numel() for p in net.backbone.parameters() if p.requires_grad))
+print("decoder : ", sum(p.numel() for p in net.decoder.parameters() if p.requires_grad))
